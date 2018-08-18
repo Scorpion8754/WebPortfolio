88ea40d1b5e42c83ea2a727e29f4f383b35fa37b
unknown <shanemain8754@gmail.com>
2018-08-17 22:59:21 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Web-Portfolio
+Web Portfolio
+
+
+Web portfoio created using MYSQL and Flask. The purpose of this is to learn Flask as an introduction to webapps before making
+more complex projects in Django. The design philosophy is to make it as easy to maintain as possible with a functional backend.
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cum iusto quos sequi voluptatibus. Aperiam commodi, cumque harum ipsum nesciunt nihil nobis nulla, perferendis quia quisquam quos repellendus soluta voluptates.',
+            'author': 'Ya Boi',
+            'create_date' : '04-25-2018'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cum iusto quos sequi voluptatibus. Aperiam commodi, cumque harum ipsum nesciunt nihil nobis nulla, perferendis quia quisquam quos repellendus soluta voluptates.',
+            'author': 'Place Holder',
+            'create_date': '04-20-2019'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cum iusto quos sequi voluptatibus. Aperiam commodi, cumque harum ipsum nesciunt nihil nobis nulla, perferendis quia quisquam quos repellendus soluta voluptates.',
+            'author': 'Donnie Tendie',
+            'create_date': '04-25-2018'
+        }
+    ]
+    return articles
