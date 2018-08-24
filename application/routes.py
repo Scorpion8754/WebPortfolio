ccab2d769a6727aec841b5de96a8570126552c2b
Scorpion8754 <35542934+Scorpion8754@users.noreply.github.com>
2018-08-23 18:07:29 -0700

    Restructure, more SQL setup

---
+++ application/routes.py
@@ -0,0 +1,64 @@
+from flask import render_template, url_for, flash, redirect
+from application import application, db, bcrypt
+from application.forms import RegistrationForm, LoginForm
+from application.models import User, Post
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+@application.route('/')
+def index():
+    return render_template('index.html', title="Home")
+
+
+@application.route('/about')
+def about():
+    return render_template('about.html', title="About")
+
+
+@application.route('/projects')
+def articles():
+    return render_template('articles.html', articles = Articles, title="Projects")
+
+
+@application.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html', id=id)
+
+
+@application.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@application.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
